a1ee5099e74cf3fcef6c0110833647a2e0d3539c
hi-dhl <hihuilong@gmail.com>
2022-07-31 18:49:50 +0800

    stype: optimize code

--- code/_1122/Solution.cpp
+++ code/_1122/Solution.cpp
@@ -1,4 +1,34 @@
 //
-// Created by denghuilong on 2022/7/31.
+// Created by DHL on 2022/7/31.
 //
 
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int> &arr1, vector<int> &arr2) {
+        int len = 1001;
+        int *result = new int[len]();
+        int alen = arr1.size();
+        for (int i = 0; i < alen; i++) {
+            result[arr1[i]]++;
+        }
+
+        int index = 0;
+        int blen = arr2.size();
+        for (int i = 0; i < blen; i++) {
+            int key = arr2[i];
+            while (result[key] > 0) {
+                arr1[index++] = key;
+                result[key]--;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            while (result[i] > 0) {
+                arr1[index++] = i;
+                result[i]--;
+            }
+        }
+        delete[] result;
+        return arr1;
+    }
+};
